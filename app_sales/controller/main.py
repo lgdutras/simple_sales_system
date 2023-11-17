a646529f3af47db0357a9d38cb869c0e6a07898c
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2023-11-17 15:13:11 -0300

    register_sale update
    -- templates folder changed
    -- remove field added to saleForm on register_sale
    -- bootstrap 4.0 included to project, some form fields got classes

--- app_sales/controller/main.py
+++ app_sales/controller/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from secrets import token_hex
 
-app = Flask(__name__, template_folder=r"C:/Users/Estoque01/Documents/GitHub/simple_sales_system/app_sales/view")
+app = Flask(__name__, template_folder=r"C:/GitHub/simple_sales_system/app_sales/view")
 app.secret_key = token_hex(64)
 
+
+# ROTAS
 @app.route('/')
 def index():
     if session.get('logged_in'):
@@ -11,7 +13,7 @@ def index():
     else:
         return redirect(url_for('login'))
 
-@app.route('/login', methods = ['Post', 'Get'])
+@app.route('/login', methods = ['Post', 'Get']) # Login Page/System
 def login():
     if request.method == 'POST':
         usuario = request.form['username']
@@ -33,6 +35,10 @@ def homepage():
         print('Acesso Negado!')
         return redirect(url_for('login'))
     
+@app.route('/register_product')
+def register_product():
+    return render_template('register_product.html')
+    
 @app.route('/logout', methods = ['Post', 'Get'])
 def logout():
     session.pop('logged_in', None)
@@ -46,10 +52,6 @@ def register_sale():
 @app.route('/view_sales')
 def view_sales():
     return render_template('view_sales.html')
-
-@app.route('/register_product')
-def register_product():
-    return render_template('register_product.html')
 
 @app.route('/register_costumer')
 def register_costumer():
