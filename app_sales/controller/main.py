d703952b20b88bb71254f8823cf5b3b4d8ce3295
Luiz Gustavo Dutra de Souza <gdutrasouza@gmail.com>
2023-11-17 17:15:15 -0300

    form+route integration
    setting parameters in register_sale.html to call on main.py and, after, pass it to database

--- app_sales/controller/main.py
+++ app_sales/controller/main.py
@@ -45,8 +45,29 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
-@app.route('/register_sale')
+@app.route('/register_sale', methods = ['Post', 'Get'])
 def register_sale():
+    if request.method == 'POST':
+        # Get header
+        itemCounter = int(request.form['itemCounter'])
+        userRegistry = str(request.form['userRegistry'])
+        employee = str(request.form['employee'])
+        # Get items
+        items = []
+        input = [] #this object gonna call a input action on database
+        for item in range(1, itemCounter+1):
+            item = {
+                'item': request.form['item%s' % item],
+                'quantity': request.form['quantity%s' % item],
+                'price': request.form['price%s' % item]
+            }
+            items.append(item)
+        for item in items:
+            # string to test if the parameters are being right called on main.py to after insert on database
+            insert = "insert into is_sales values (%s, %s, %s, %s, %s)" % (session.get('username'), userRegistry, item['item'], item['quantity'], item['price'])
+            input.append(insert)
+        return input
+    
     return render_template('register_sale.html')
 
 @app.route('/view_sales')
