6b12e9591287db7aabaad12c525d5fa233c04b62
Luiz Gustavo Dutra de Souza <113903518+lgdutras@users.noreply.github.com>
2023-11-30 21:39:30 -0300

    Delete model/database/dbconfig.py
    remove file that i want to ignore

--- model/database/dbconfig.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-
-login = 'x'
-password = 'x'
-hostname = 'x'
-conn_string = 'oracle+cx_oracle://%s:%s@%s' % (login, password, hostname)
-engine = create_engine(conn_string)
